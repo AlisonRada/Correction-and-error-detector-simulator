1ec3d8350fff3536b019e1c8e7a3d9c06ee15f20
Alison Rada <ralison@uninorte.edu.co>
2019-03-24 12:14:11 -0500

    First Version

---
+++ src/Controlador/Main.java
@@ -0,0 +1,21 @@
+
+package Controlador;
+
+import Vista.Home;
+import Vista.Inicio;
+
+/**
+ *
+ * @author Alison
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Inicio h = new Inicio();
+       h.setVisible(true);
+    }
+    
+}
