8b18494619c3cd8fed92ba0c69b0d0a44008d585
Alison Rada <ralison@uninorte.edu.co>
2019-03-24 12:14:11 -0500

    Schema window. Deleted window Home example.

---
+++ src/Controlador/Main.java
@@ -0,0 +1,20 @@
+
+package Controlador;
+
+import Vista.Home;
+
+/**
+ *
+ * @author Alison
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Home h = new Home();
+       h.setVisible(true);
+    }
+    
+}
